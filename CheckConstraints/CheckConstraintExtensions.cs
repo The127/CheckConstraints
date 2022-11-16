73c56e41e6943751cbd3a24b1740dc85c2cfefbf
the127 <julian.baehr@googlemail.com>
2022-11-16 18:22:32 +0100

    Real initial commit

---
+++ CheckConstraints/CheckConstraintExtensions.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Darkarotte.CheckConstraints;
+
+public static class CheckConstraintExtensions
+{
+    public static DbContextOptionsBuilder UseCheckConstraints(
+        this DbContextOptionsBuilder optionsBuilder)
+    {
+        var extension = optionsBuilder.Options.FindExtension<CheckConstraintExtension>() ??
+                        new CheckConstraintExtension();
+
+        var dbContextOptionsBuilderInfrastructure = (IDbContextOptionsBuilderInfrastructure)optionsBuilder;
+        dbContextOptionsBuilderInfrastructure.AddOrUpdateExtension(extension);
+
+        return optionsBuilder;
+    }
+}
+
+public class CheckConstraintExtension : IDbContextOptionsExtension
+{
+    public DbContextOptionsExtensionInfo Info { get; }
+
+    public CheckConstraintExtension()
+    {
+        Info = new ExtensionInfo(this);
+    }
+
+    public void ApplyServices(IServiceCollection services)
+    {
+        var entityFrameworkServicesBuilder = new EntityFrameworkServicesBuilder(services);
+        entityFrameworkServicesBuilder.TryAdd<IConventionSetPlugin, CheckConstraintService>();
+    }
+
+    public void Validate(IDbContextOptions options)
+    {
+    }
+}
+
+public class CheckConstraintService : IConventionSetPlugin
+{
+    public ConventionSet ModifyConventions(ConventionSet conventionSet)
+    {
+        conventionSet.ModelFinalizingConventions.Add(new CheckConstraintConvention());
+        return conventionSet;
+    }
+}
+
+public class CheckConstraintConvention : IModelFinalizingConvention
+{
+    public void ProcessModelFinalizing(
+        IConventionModelBuilder modelBuilder,
+        IConventionContext<IConventionModelBuilder> context)
+    {
+        foreach (var conventionEntityType in modelBuilder.Metadata.GetEntityTypes())
+        {
+            var entityClrType = conventionEntityType.ClrType;
+
+            if (!ConstraintsBuilder.TryGetConstraints(entityClrType, out var constraints)) 
+                continue;
+            
+            foreach (var constraint in constraints)
+            {
+                constraint.Configure(modelBuilder);
+            }
+        }
+    }
+}
+
+internal sealed class ExtensionInfo : DbContextOptionsExtensionInfo
+{
+    public ExtensionInfo(IDbContextOptionsExtension extension)
+        : base(extension)
+    {
+    }
+
+    public override bool IsDatabaseProvider => false;
+
+    public override string LogFragment => "using check constraints";
+
+    public override int GetServiceProviderHashCode()
+        => 0;
+
+    public override bool ShouldUseSameServiceProvider(DbContextOptionsExtensionInfo other)
+        => other is ExtensionInfo;
+
+    public override void PopulateDebugInfo(IDictionary<string, string> debugInfo)
+    {
+        debugInfo["CheckConstraint:All"] = "1";
+    }
+}
---
+++ CheckConstraints/ConstraintsBuilder.cs
@@ -0,0 +1,231 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Darkarotte.CheckConstraints;
+
+public abstract class CheckConstraint
+{
+    protected readonly PropertyBuilder PropertyBuilder;
+
+    public CheckConstraint(PropertyBuilder propertyBuilder)
+    {
+        PropertyBuilder = propertyBuilder;
+    }
+
+    public abstract void Configure(IConventionModelBuilder builder);
+
+    public string GetCheckConstraintName()
+        => $"CK_" +
+           $"{PropertyBuilder.Metadata.DeclaringEntityType.GetTableName()}" +
+           $"_" +
+           $"{PropertyBuilder.Metadata.GetColumnName()}" +
+           $"_" +
+           $"{ConstraintTypeName}";
+
+    public string ConstraintTypeName
+        => GetType()
+            .Name
+            .Replace("Check", "")
+            .Replace("Constraint", "");
+}
+
+internal class EnumConstraint : CheckConstraint
+{
+    public EnumConstraint(PropertyBuilder propertyBuilder)
+        : base(propertyBuilder)
+    {
+    }
+
+    public override void Configure(IConventionModelBuilder builder)
+    {
+        var entityTypeBuilder = builder.Entity(PropertyBuilder.Metadata.DeclaringEntityType.ClrType)!;
+        var columnName = PropertyBuilder.Metadata.GetColumnName();
+
+        var converter = PropertyBuilder.Metadata.GetValueConverter();
+
+        // get enum values and convert them to the database type as a linq expression
+        var enumValues = Enum.GetValues(PropertyBuilder.Metadata.ClrType)
+            .Cast<object>()
+            .Select(x => converter?.ConvertToProvider(x) ?? x)
+            .Select(x => x is string s ? $"'{s}'" : x.ToString());
+
+        var sql = new StringBuilder();
+
+        if (PropertyBuilder.Metadata.ClrType == typeof(string) && PropertyBuilder.Metadata.IsNullable)
+            sql.Append($"(\"{columnName}\" IS NULL OR ");
+
+        sql.Append($"\"{columnName}\" IN (");
+        sql.AppendJoin(", ", enumValues);
+        sql.Append(")");
+
+        entityTypeBuilder.HasCheckConstraint(GetCheckConstraintName(), sql.ToString());
+    }
+}
+
+public class RegexConstraint : CheckConstraint
+{
+    private readonly string _regex;
+
+    public RegexConstraint(PropertyBuilder propertyBuilder, string regex)
+        : base(propertyBuilder)
+    {
+        _regex = regex;
+    }
+
+    public override void Configure(IConventionModelBuilder builder)
+    {
+        var entityTypeBuilder = builder.Entity(PropertyBuilder.Metadata.DeclaringEntityType.ClrType)!;
+        var columnName = PropertyBuilder.Metadata.GetColumnName();
+
+        var sqlBuilder = new StringBuilder();
+
+        if (PropertyBuilder.Metadata.ClrType == typeof(string) && PropertyBuilder.Metadata.IsNullable)
+            sqlBuilder.Append($"(\"{columnName}\" IS NULL OR ");
+
+        sqlBuilder.Append($"\"{columnName}\" ~ '{_regex}'");
+
+        entityTypeBuilder.HasCheckConstraint(GetCheckConstraintName(), sqlBuilder.ToString());
+    }
+}
+
+public class LengthConstraint : CheckConstraint
+{
+    private readonly uint _maxLength;
+    private readonly uint? _minLength;
+
+    public LengthConstraint(PropertyBuilder propertyBuilder, uint maxLength, uint? minLength)
+        : base(propertyBuilder)
+    {
+        _maxLength = maxLength;
+        _minLength = minLength;
+    }
+
+    public override void Configure(IConventionModelBuilder builder)
+    {
+        var entityTypeBuilder = builder.Entity(PropertyBuilder.Metadata.DeclaringEntityType.ClrType)!;
+        var columnName = PropertyBuilder.Metadata.GetColumnName();
+
+        var sqlBuilder = new StringBuilder();
+
+        if (PropertyBuilder.Metadata.ClrType == typeof(string) && PropertyBuilder.Metadata.IsNullable)
+            sqlBuilder.Append($"(\"{columnName}\" IS NULL OR ");
+
+        sqlBuilder.Append($"LENGTH(\"{columnName}\") <= {_maxLength}");
+        if (_minLength.HasValue)
+        {
+            sqlBuilder.Append($" AND LENGTH(\"{columnName}\") >= {_minLength}");
+        }
+
+        entityTypeBuilder.HasCheckConstraint(GetCheckConstraintName(), sqlBuilder.ToString());
+    }
+}
+
+public static class ConstraintsBuilder
+{
+    private static readonly Dictionary<Type, List<CheckConstraint>> Constraints = new();
+
+    internal static bool TryGetConstraints(Type type, [NotNullWhen(true)] out List<CheckConstraint>? constraints)
+    {
+        return Constraints.TryGetValue(type, out constraints);
+    }
+
+    public static PropertyBuilder<T> HasCustomConstraint<T>(
+        this PropertyBuilder<T> propertyBuilder, Func<PropertyBuilder<T>, CheckConstraint> constraintFactory)
+    {
+        var constraint = constraintFactory(propertyBuilder);
+        AddConstraint(propertyBuilder, constraint);
+        return propertyBuilder;
+    }
+
+    public static PropertyBuilder<string> HasRegexConstraint(this PropertyBuilder<string> propertyBuilder, string regex)
+    {
+        InternalHasRegexConstraint(propertyBuilder, regex);
+        return propertyBuilder;
+    }
+    
+    public static PropertyBuilder<string?> HasNullableRegexConstraint(this PropertyBuilder<string?> propertyBuilder, string regex)
+    {
+        InternalHasRegexConstraint(propertyBuilder, regex);
+        return propertyBuilder;
+    }
+    
+    private static void InternalHasRegexConstraint(this PropertyBuilder propertyBuilder, string regex)
+    {
+        var constraint = new RegexConstraint(propertyBuilder, regex);
+        AddConstraint(propertyBuilder, constraint);
+    }
+
+    public static PropertyBuilder<string> HasLengthConstraint(this PropertyBuilder<string> propertyBuilder,
+        uint maxLength, uint? minLength = null)
+    {
+        InternalHasLengthConstraint(propertyBuilder, maxLength, minLength);
+        return propertyBuilder;
+    }
+
+    public static PropertyBuilder<string?> HasNullableLengthConstraint(this PropertyBuilder<string?> propertyBuilder,
+        uint maxLength, uint? minLength = null)
+    {
+        InternalHasLengthConstraint(propertyBuilder, maxLength, minLength);
+        return propertyBuilder;
+    }
+
+    private static void InternalHasLengthConstraint(PropertyBuilder propertyBuilder, uint maxLength,
+        uint? minLength = null)
+    {
+        var constraint = new LengthConstraint(propertyBuilder, maxLength, minLength);
+        AddConstraint(propertyBuilder, constraint);
+    }
+
+    public static PropertyBuilder<T> HasEnumConstraint<T>(this PropertyBuilder<T> propertyBuilder)
+        where T : struct, Enum
+    {
+        InternalHasEnumConstraint(propertyBuilder);
+        return propertyBuilder;
+    }
+
+    public static PropertyBuilder<T?> HasNullableEnumConstraint<T>(this PropertyBuilder<T?> propertyBuilder)
+        where T : struct, Enum
+    {
+        InternalHasEnumConstraint(propertyBuilder);
+        return propertyBuilder;
+    }
+
+    private static void InternalHasEnumConstraint(this PropertyBuilder propertyBuilder)
+    {
+        var constraint = new EnumConstraint(propertyBuilder);
+        AddConstraint(propertyBuilder, constraint);
+    }
+
+    public static PropertyBuilder<string> HasEmailConstraint(this PropertyBuilder<string> propertyBuilder)
+    {
+        InternalHasEmailConstraint(propertyBuilder);
+        return propertyBuilder;
+    }
+
+    public static PropertyBuilder<string?> HasNullableEmailConstraint(this PropertyBuilder<string?> propertyBuilder)
+    {
+        InternalHasEmailConstraint(propertyBuilder);
+        return propertyBuilder;
+    }
+
+    public static string EmailRegex => @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,5})+)$";
+
+    private static void InternalHasEmailConstraint(this PropertyBuilder propertyBuilder)
+    {
+        var constraint = new RegexConstraint(propertyBuilder, EmailRegex);
+        AddConstraint(propertyBuilder, constraint);
+    }
+
+    private static void AddConstraint(PropertyBuilder propertyBuilder, CheckConstraint constraint)
+    {
+        var entityType = propertyBuilder.Metadata.DeclaringEntityType.ClrType;
+        if (!Constraints.ContainsKey(entityType))
+        {
+            Constraints.Add(entityType, new List<CheckConstraint>());
+        }
+
+        Constraints[entityType].Add(constraint);
+    }
+}
